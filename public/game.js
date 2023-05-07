0c88a69acb0e2c4e9b8f39e980e19c71dcda5202
Tony Paik <tonypaik@Tonys-MacBook-Pro.local>
2023-05-06 22:12:27 -0700

    dice

--- public/game.js
+++ public/game.js
@@ -13,22 +13,62 @@ let config = {
 let game = new Phaser.Game(config);
 
 // initialize variables
+const gamePath = [
+  "sun",
+  "star1",
+  "star2",
+  "mercury",
+  "star3",
+  "star4",
+  "venus",
+  "star5",
+  "star6",
+  "earth",
+  "star7",
+  "star8",
+  "mars",
+  "star9",
+  "star10",
+  "jupiter",
+  "star11",
+  "star12",
+  "saturn",
+  "star13",
+  "star14",
+  "uranus",
+  "star15",
+  "star16",
+  "neptune",
+  "star17",
+  "star18",
+];
 
 // Load assets
 gameScene.preload = function () {
   // load images
   this.load.image("background", "assets/background.png");
   this.load.image("finalcompleteboard", "assets/finalcompleteboard.png");
-  this.load.image("earth", "assets/earth.png");
   this.load.image("sun", "assets/sun.png");
   this.load.image("mercury", "assets/mercury.png");
   this.load.image("venus", "assets/venus.png");
+  this.load.image("earth", "assets/earth.png");
   this.load.image("mars", "assets/mars.png");
   this.load.image("jupiter", "assets/jupiter.png");
   this.load.image("saturn", "assets/saturn.png");
   this.load.image("uranus", "assets/uranus.png");
   this.load.image("neptune", "assets/neptune.png");
   this.load.image("star", "assets/star.png");
+  this.load.image("roll", "assets/roll.png");
+  this.load.image("diceone", "assets/diceone.png");
+  this.load.image("dicetwo", "assets/dicetwo.png");
+  this.load.image("dicethree", "assets/dicethree.png");
+  this.load.image("dicefour", "assets/dicefour.png");
+  this.load.image("dicefive", "assets/dicefive.png");
+  this.load.image("dicesix", "assets/dicesix.png");
+  this.load.image("playerone", "assets/playerone.png");
+  this.load.image("playertwo", "assets/playertwo.png");
+  this.load.image("playerthree", "assets/playerthree.png");
+  this.load.image("playerfour", "assets/playerfour.png");
   // this.load.image("meteorite", "assets/meteorite.png");
   // this.load.image("satellite", "assets/satellite.png");
   // this.load.image("startButton", "assets/startButton.png");
@@ -347,7 +387,51 @@ gameScene.create = function () {
       this.sys.game.config.width / 2 - 240,
       this.sys.game.config.height / 2 + 73
     );
+
+  // Create roll button and make it interactive
+  this.rollButton = this.add
+    .sprite(0, 0, "roll")
+    .setScale(1.25, 1.25)
+    .setPosition(
+      this.sys.game.config.width / 2,
+      this.sys.game.config.height / 2
+    )
+    .setInteractive();
+  this.rollButton.on("pointerdown", this.onRollButtonClick, this);
+
+  // Create dice sprite at the center
+  this.dice = this.add.sprite(0, 0, "diceone");
+  this.dice.setScale(1.5, 1.5);
+  this.dice.setPosition(
+    this.sys.game.config.width / 2,
+    this.sys.game.config.height / 2
+  );
+  this.dice.setVisible(false);
+
+  const worldWidth = this.sys.game.config.width;
+  const worldHeight = this.sys.game.config.height;
+
+  // Position the markers in each corner of the game world
+  const playerOneMarker = this.add
+    .sprite(0, 0, "playerone")
+    .setOrigin(0, 0)
+    .setScale(2, 2);
+  const playerTwoMarker = this.add
+    .sprite(worldWidth, 0, "playertwo")
+    .setOrigin(1, 0)
+    .setScale(2, 2);
+  const playerThreeMarker = this.add
+    .sprite(0, worldHeight, "playerthree")
+    .setOrigin(0, 1)
+    .setScale(2, 2);
+  const playerFourMarker = this.add
+    .sprite(worldWidth, worldHeight, "playerfour")
+    .setOrigin(1, 1)
+    .setScale(2, 2);
 };
+
+// this is called up to 60 times per second
+gameScene.update = function () {};
 
 gameScene.onSunClick = function () {
   window.location.href = "https://jamieskidmore.github.io/clicker/";
@@ -366,5 +450,36 @@ gameScene.onuranusClick = function () {
   window.location.href = "https://qubeqode.github.io/phaserMemoryGame/";
 };
 
-// this is called up to 60 times per second
-gameScene.update = function () {};
+gameScene.onRollButtonClick = function () {
+  this.rollButton.setVisible(false);
+  this.dice.setVisible(true);
+  this.rollDice();
+};
+
+gameScene.rollDice = function () {
+  // Start dice rolling animation
+  let diceAnimation = this.time.addEvent({
+    delay: 100,
+    callback: this.randomizeDiceFace,
+    callbackScope: this,
+    loop: true,
+  });
+
+  // Stop dice rolling animation after 2 seconds
+  this.time.delayedCall(2000, () => {
+    diceAnimation.remove();
+  });
+};
+
+gameScene.randomizeDiceFace = function () {
+  let diceFaces = [
+    "diceone",
+    "dicetwo",
+    "dicethree",
+    "dicefour",
+    "dicefive",
+    "dicesix",
+  ];
+  let randomFace = Phaser.Math.RND.pick(diceFaces);
+  this.dice.setTexture(randomFace);
+};
